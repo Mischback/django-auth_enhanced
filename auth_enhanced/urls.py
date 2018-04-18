d8e7ae3a20309b647ae427eb2a59d6254359daf7
Mischback <dev@mischback.de>
2018-04-18 07:56:39 +0200

    Made logout work

--- auth_enhanced/urls.py
+++ auth_enhanced/urls.py
@@ -12,13 +12,14 @@ may have to adjust your imports accordingly."""
 
 # Django imports
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # specify the app name
 app_name = 'auth_enhanced'
 
 # define the urls to match
+# TODO: 'url' got replaced by 'path' in Django 2.0
 urlpatterns = [
-    # TODO: 'url' got replaced by 'path' in Django 2.0
     url(r'^login/$', LoginView.as_view(template_name='auth_enhanced/login.html'), name='login'),
+    url(r'^logout/$', LogoutView.as_view(template_name='auth_enhanced/logout.html'), name='logout'),
 ]
