8ae56a90e88f57121d3412d1a56d7c43e7cdc6ab
Mischback <dev@mischback.de>
2018-04-24 01:23:21 +0200

    Created custom wrapper to apply ModelAdmin only if DEBUG=True
    This code works, but is not tested...

--- auth_enhanced/admin.py
+++ auth_enhanced/admin.py
@@ -3,13 +3,33 @@
 
 # Django imports
 from django.conf import settings
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
 # app imports
 from auth_enhanced.models import UserEnhancement
 
 
-class UserEnhancementAdmin(admin.ModelAdmin):
+def register_only_debug(*models, site=None):
+    """Register the given model(s) classes and wrapped ModelAdmin class with
+    admin site, if DEBUG=True in project's settings.
+
+    See https://github.com/django/django/blob/master/django/contrib/admin/decorators.py
+    for the original Django implementation."""
+
+    # need a callable here, but just 'pass'ing is fine...
+    def _wrapper_noop(admin_class):
+        pass
+
+    if settings.DEBUG:
+        # re-use Django's register-decorator
+        return register(*models, site=site)
+
+    # return a noop
+    return _wrapper_noop
+
+
+@register_only_debug(UserEnhancement)
+class UserEnhancementAdmin(ModelAdmin):
     """Integrates UserEnhancement into Django's admin menu.
 
     This ModelAdmin is just used for development and should not be registered
@@ -17,8 +37,3 @@ class UserEnhancementAdmin(admin.ModelAdmin):
     UserEnhancements will be integrated into the respective admin class for
     the User-objects."""
     pass
-
-
-# register UserEnhancementAdmin only in DEBUG-mode
-if settings.DEBUG:
-    admin.site.register(UserEnhancement, UserEnhancementAdmin)
