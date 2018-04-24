7b44c6ebec6e7606e28a3084d712665de3fcce45
Mischback <dev@mischback.de>
2018-04-24 22:20:44 +0200

    Created email abstraction layer

---
+++ auth_enhanced/email.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Handles all email-related stuff of the app."""
+
+
+# Django imports
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+
+# app imports
+from auth_enhanced.exceptions import AuthEnhancedException
+
+
+class AuthEnhancedEmail(EmailMultiAlternatives):
+    """Base class for all app-related email messages."""
+
+    def __init__(self, txt_template=None, html_template=None, context=None, **kwargs):
+
+        # remove the body, because the app relies on templates instead
+        try:
+            del kwargs['body']
+        except KeyError:
+            # 'body' was not specified, so we just 'pass'
+            pass
+
+        # call the parent constructor
+        super(AuthEnhancedEmail, self).__init__(**kwargs)
+
+        # check for 'txt_template'
+        # TODO: Is this really a minimum requirement?
+        if not txt_template:
+            raise self.AuthEnhancedEmailException(_("A 'txt_template' must be provided!"))
+
+        # check for context (required for rendering)
+        # TODO: Can this be handled more graceful? I.e by defaulting 'context' to {}?
+        if context is not None and not isinstance(context, dict):
+            raise self.AuthEnhancedEmailException(_("A 'context' must be provided!"))
+
+        # render and attach the 'txt_body'
+        txt_body = render_to_string('auth_enhanced/mail/{}'.format(txt_template), context)
+        self.body = txt_body
+
+        # render an alternative 'html_body'
+        if html_template:
+            html_body = render_to_string('auth_enhanced/mail/{}'.format(html_template), context)
+            self.attach_alternative(html_body, 'text/html')
+
+    class AuthEnhancedEmailException(AuthEnhancedException):
+        """This exception indicates, that something went wrong inside the class."""
+        pass
