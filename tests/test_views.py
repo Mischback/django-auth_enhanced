7ecb056639e69b87a4620721d8f874728820a2d1
Mischback <dev@mischback.de>
2018-05-02 19:10:02 +0200

    Added tests for 'views.EmailVerificationView.get()'

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import get_user_model
 from django.test import override_settings, RequestFactory, tag  # noqa
 
 # app imports
+from auth_enhanced.exceptions import AuthEnhancedException
 from auth_enhanced.forms import EmailVerificationForm
 from auth_enhanced.views import EmailVerificationView
 
@@ -51,8 +52,20 @@ class EmailVerificationViewTests(AuthEnhancedTestCase):
         def activate_user_does_not_exist(mock_obj):
             raise get_user_model().DoesNotExist('bar')
 
+        @staticmethod
+        def is_valid_true(mock_obj):
+            return True
+
+        @staticmethod
+        def is_valid_false(mock_obj):
+            return False
+
+        @staticmethod
+        def raise_something(mock_obj):
+            raise AuthEnhancedException('bar')
+
     @mock.patch('auth_enhanced.forms.EmailVerificationForm.activate_user')
-    def test_form_valid_ok(self, mock):
+    def test_form_valid_ok(self, mock_func):
         """Calling 'form_valid()' should activate the user by using the form's
         method 'activate_user'."""
 
@@ -63,7 +76,7 @@ class EmailVerificationViewTests(AuthEnhancedTestCase):
 
         view.form_valid(view.get_form())
 
-        self.assertTrue(mock.called)
+        self.assertTrue(mock_func.called)
 
     @mock.patch(
         'auth_enhanced.forms.EmailVerificationForm.activate_user',
@@ -79,3 +92,52 @@ class EmailVerificationViewTests(AuthEnhancedTestCase):
         )
         with self.assertRaises(get_user_model().DoesNotExist):
             view.form_valid(view.get_form())
+
+    @mock.patch('auth_enhanced.forms.EmailVerificationForm.is_valid', MockEmailVerificationForm.is_valid_true)
+    @mock.patch('auth_enhanced.views.EmailVerificationView.form_valid')
+    def test_get_verification_token_valid(self, mock_func):
+        """Call the view with a token in the url should verify the token."""
+
+        view = setup_view(
+            EmailVerificationView(),
+            RequestFactory().get('/foo/')
+        )
+
+        view.get(
+            RequestFactory().get('/foo/'),
+            verification_token='foo'
+        )
+
+        self.assertTrue(mock_func.called)
+
+    @mock.patch('auth_enhanced.forms.EmailVerificationForm.is_valid', MockEmailVerificationForm.is_valid_false)
+    @mock.patch('auth_enhanced.views.redirect', MockEmailVerificationForm.raise_something)
+    def test_get_verification_token_invalid(self):
+        """Call the view with a token in the url should verify the token.
+
+        Please notice the mock of 'redirect'. The method is mocked in its
+        imported context (in 'auth_enhanced.views')."""
+
+        view = setup_view(
+            EmailVerificationView(),
+            RequestFactory().get('/foo/')
+        )
+
+        with self.assertRaises(AuthEnhancedException):
+            retval = view.get(                  # noqa
+                RequestFactory().get('/foo/'),  # noqa
+                verification_token='foo'        # noqa
+            )                                   # noqa
+
+    @mock.patch('django.views.generic.edit.ProcessFormView.get')
+    def test_get_plain(self, mock_func):
+        """Call the view without a token."""
+
+        view = setup_view(
+            EmailVerificationView(),
+            RequestFactory().get('/foo/')
+        )
+
+        view.get(RequestFactory().get('/foo/'))
+
+        self.assertTrue(mock_func)
