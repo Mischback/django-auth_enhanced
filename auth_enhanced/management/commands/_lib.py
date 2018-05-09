5e505515d8c46e5ee35c2be2c849827add208d0d
Mischback <dev@mischback.de>
2018-05-09 22:30:03 +0200

    Included another abstraction to not reference any field names

--- auth_enhanced/management/commands/_lib.py
+++ auth_enhanced/management/commands/_lib.py
@@ -10,23 +10,34 @@ from django.db.models import Count
 def check_email_uniqueness(stdout):
     """This function checks, if all email addresses are unique."""
 
+    user_model = get_user_model()
+
     # find the email addresses, that are not unique
-    non_unique_mails = (
-        get_user_model().objects.values('email')
-        .annotate(count=Count('id'))
+    non_unique_emails = (
+        # get all email addresses
+        user_model.objects.values(user_model.EMAIL_FIELD)
+        # annotate them by counting a guaranteed unique field
+        .annotate(count=Count('pk'))
+        # get all email addresses, that appear more than one time
         .filter(count__gt=1)
-        .values('email')
+        # remove the annotation again
+        .values(user_model.EMAIL_FIELD)
     )
 
-    if not non_unique_mails:
+    if not non_unique_emails:
         # all email addresses are unique!
         stdout.write('[ok] All email addresses are unique!')
     else:
 
         # get the usernames of accounts, that have non-unique email addresses
         non_unique_accounts = (
-            get_user_model().objects.filter(email__in=non_unique_mails)
-            .values_list('username', flat=True)
+            user_model.objects.filter(
+                # this really complex statement is used, to not reference the
+                #   'email' field directly, to be as pluggable as possible
+                **{ '{}__in'.format(user_model.EMAIL_FIELD): non_unique_emails }
+            )
+            # simply list the usernames
+            .values_list(user_model.USERNAME_FIELD, flat=True)
         )
 
         # raise an Error and include a list of usernames without unique email
